15c76b019eee737faa145f0f7c42eb5b99cab187
SikuDev <68606343+SikuDev@users.noreply.github.com>
2022-12-16 20:50:48 -0500

    Cat Meme Picker

---
+++ module-5-essential-javascript/meme-generator/index.js
@@ -0,0 +1,63 @@
+import { catsData } from '/data.js'
+
+const animatedBtn = document.getElementById('animated-btn')
+const memeModalInner = document.getElementById('meme-modal-inner')
+const memeModal = document.getElementById('meme-modal')
+const memeModalCloseBtn = document.getElementById('meme-modal-close-btn')
+const moodBtns = document.querySelectorAll('.mood-btn')
+
+let isGif = false
+
+memeModalCloseBtn.addEventListener('click', closeModal)
+
+animatedBtn.addEventListener('click', function(e){
+  if (isGif){
+    isGif = false
+    this.firstElementChild.textContent = ''
+    console.log(this);
+  } else {
+    isGif = true
+    this.firstElementChild.textContent = '✓'
+    console.log(this);
+  }
+})
+
+for (let btn of moodBtns) {
+  btn.addEventListener('click', function () {
+    const selectedMood = this.textContent.trim()
+    // Get matching objects
+    const matchingCatsArray = getMatchingCatsArray(selectedMood)
+    // Pick on randomly
+    const randomIndex = Math.floor(Math.random() * matchingCatsArray.length)
+    // Render
+    renderCat(matchingCatsArray[randomIndex])
+  })
+}
+
+
+function closeModal() {
+  memeModal.style.display = 'none'
+}
+
+function renderCat(catObject) {
+  memeModalInner.innerHTML = `
+        <img 
+        class="cat-img" 
+        src="./images/${catObject.image}"
+        alt="${catObject.alt}"
+        >
+        `
+  memeModal.style.display = 'flex'
+}
+
+function getMatchingCatsArray(mood) {
+  const matchingCatsArray = catsData.filter(function (cat) {
+    if (isGif) {
+      return cat.emotionTags.includes(mood) && cat.isGif
+    }
+    else {
+      return cat.emotionTags.includes(mood)
+    }
+  })
+  return matchingCatsArray
+}  
